ee2c64f0bcb63c10fc87cb8a5d2924a85de345ad
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-01 20:01:02 +0400

    Create BlogPost model

--- Lecture code 1/blog_post/blog/models.py
+++ Lecture code 1/blog_post/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(verbose_name="სათაური", max_length=255)
+    text = models.TextField(verbose_name="ტექსი")
+
+    class Meta:
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+
+    def __str__(self):
+        return self.title
