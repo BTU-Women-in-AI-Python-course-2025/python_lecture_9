f26c8a3245e4e5b2be5c2566bccb0551e465d5a0
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-02 20:32:00 +0400

    Add some fields in model

--- Lecture code 2/blog_post/blog/models.py
+++ Lecture code 2/blog_post/blog/models.py
@@ -4,6 +4,14 @@ from django.db import models
 class BlogPost(models.Model):
     title = models.CharField(verbose_name='სათაური', max_length=255)
     text = models.TextField(verbose_name='ტექსტი')
+    is_active = models.BooleanField(verbose_name='აქტიურია', default=True)
+    created_at = models.DateTimeField(
+        verbose_name='შექმნის თარიღი', auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(
+        verbose_name='განახლების თარიღი', auto_now=True, null=True)
+    website = models.URLField(verbose_name='ვებ მისამართი', null=True)
+    document = models.FileField(upload_to='blog_post_documents/', null=True)
+    picture = models.ImageField(upload_to='blog_post_picture/', null=True)
 
     class Meta:
         verbose_name = "Blog Post"
